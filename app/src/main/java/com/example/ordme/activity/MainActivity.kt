4e1e49eafc149c26f19c3be7f6eaed078416ccfe
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2022-07-10 15:45:24 +0200

    init project and firebase

---
+++ app/src/main/java/com/example/ordme/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ordme.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ordme.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/ordme/screen/MainFragment.kt
@@ -0,0 +1,16 @@
+package com.example.ordme.screen
+
+import com.example.ordme.R
+import com.example.ordme.base.BaseFragment
+
+class MainFragment: BaseFragment() {
+    override val layout: Int = R.layout.fragment_main
+
+    override fun subscribeUi() {
+
+    }
+
+    override fun unsubscribeUi() {
+
+    }
+}
