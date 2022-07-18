c623d40c1c2a026e6bbe682c7ca0dd463ff682ad
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2022-07-18 20:10:57 +0200

    create fragments, navigation and menu

---
+++ app/src/main/java/com/example/ordme/screen/restaurant/RegisterRestaurantFragment.kt
@@ -0,0 +1,16 @@
+package com.example.ordme.screen.restaurant
+
+import com.example.ordme.R
+import com.example.ordme.base.BaseFragment
+
+class RegisterRestaurantFragment: BaseFragment() {
+    override val layout: Int = R.layout.fragment_register_restaurant
+
+    override fun subscribeUi() {
+
+    }
+
+    override fun unsubscribeUi() {
+
+    }
+}
---
+++ app/src/main/java/com/example/ordme/screen/user/RegisterUserFragment.kt
@@ -0,0 +1,16 @@
+package com.example.ordme.screen.user
+
+import com.example.ordme.R
+import com.example.ordme.base.BaseFragment
+
+class RegisterUserFragment: BaseFragment() {
+    override val layout: Int = R.layout.fragment_register_user
+
+    override fun subscribeUi() {
+
+    }
+
+    override fun unsubscribeUi() {
+
+    }
+}
