d9da0da68425c5505c9beff441c0043d510b537c
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2022-08-25 22:33:34 +0200

    add drawer

--- app/src/main/java/com/example/ordme/activity/MainActivity.kt
+++ app/src/main/java/com/example/ordme/activity/MainActivity.kt
@@ -2,11 +2,70 @@ package com.example.ordme.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.ordme.R
+import com.example.ordme.ui.restaurant.screen.LoginRestaurantFragment
+import com.example.ordme.ui.restaurant.screen.RegisterRestaurantFragment
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawer()
+    }
+
+    private fun drawer() {
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        val drawView: NavigationView = findViewById(R.id.draw_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        drawView.setNavigationItemSelectedListener {
+
+            it.isChecked = true
+
+            when(it.itemId){
+
+                R.id.nav_home -> replaceFragment(LoginRestaurantFragment(), it.title.toString())
+                R.id.nav_message -> replaceFragment(RegisterRestaurantFragment(), it.title.toString())
+
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment, title: String){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment, fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (toggle.onOptionsItemSelected(item)){
+
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
