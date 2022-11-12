185ac9e6b762bfc3e13611f43f58f2da76ecda35
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2022-11-12 12:18:52 +0100

    easier to use Parcelable

--- app/src/main/java/com/example/ordme/ui/data/Meal.kt
+++ app/src/main/java/com/example/ordme/ui/data/Meal.kt
@@ -1,45 +1,13 @@
 package com.example.ordme.ui.data
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Meal (
     val name: String? = null,
     var amount: Int? = null,
     var price: Double? = null,
     val uidMeal: String? = null,
     val uidRestaurant: String? = null
-    ) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readValue(Double::class.java.classLoader) as? Double,
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-
-        dest.writeString(name)
-        dest.writeInt(amount!!)
-        dest.writeDouble(price!!)
-        dest.writeString(uidMeal)
-        dest.writeString(uidRestaurant)
-    }
-
-    companion object CREATOR : Parcelable.Creator<Meal> {
-        override fun createFromParcel(parcel: Parcel): Meal {
-            return Meal(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Meal?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    ) : Parcelable {}
