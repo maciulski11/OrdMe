48577997ef68f94c45c07d6f108dde2f5a9acac8
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2022-12-06 20:49:44 +0100

    implement profile fragment

---
+++ app/src/main/java/com/example/ordme/ui/screen/ProfileFragment.kt
@@ -0,0 +1,16 @@
+package com.example.ordme.ui.screen
+
+import com.example.ordme.R
+import com.example.ordme.base.BaseFragment
+
+class ProfileFragment: BaseFragment() {
+    override val layout: Int = R.layout.fragment_profile
+
+    override fun subscribeUi() {
+
+    }
+
+    override fun unsubscribeUi() {
+
+    }
+}
